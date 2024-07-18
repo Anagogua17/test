e4a8c98e23e35fd11552876b3cd9a65a1620d218
Ana Gogua <a.gogua96@gmail.com>
2024-07-18 16:30:29 +0400

    Add 1

--- Atest.py
+++ Atest.py
@@ -1,2 +1,10 @@
 start_number = int(input("E: "))
 End_number = int(input("F: "))
+
+i = start_number
+
+if start_number < End_number:
+    while i < End_number:
+        if i % 3 == 0:  
+            print(i)
+        i += 2 
